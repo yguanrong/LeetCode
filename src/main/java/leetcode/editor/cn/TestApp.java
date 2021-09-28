4317574b431e3f26ec3923da73409b79e9140d35
Y_guanrong@163.com <18776525142abcd>
2021-09-28 10:19:46 +0800

    feat:添加第31题题目解答

--- src/main/java/leetcode/editor/cn/TestApp.java
+++ src/main/java/leetcode/editor/cn/TestApp.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -13,26 +15,87 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class TestApp {
     public static void main(String[] args) {
-        AtomicDemo ad = new AtomicDemo();
-        for (int i = 0; i < 10; i++) {
-            new Thread(ad).start();
+//        int n = 0;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("请输入个数： " );
+//        n = Integer.parseInt(scanner.nextLine());
+//
+//        List<Person> people = new ArrayList<>(n);
+//
+//        System.out.println("请输入姓名和年龄用逗号隔开，一行是一个完整的人员数据：" );
+//        int index = 0;
+//        while (scanner.hasNext()){
+//            Person person = new Person();
+//            String line = scanner.nextLine();
+//            String[] lines = line.split(",");
+//            person.setName(lines[0]);
+//            person.setAge(lines[1]);
+//            people.add(person);
+//
+//            index++;
+//            if (index == n){
+//                break;
+//            }
+//        }
+//
+//
+//        System.out.println("一共输入了数据有：" + n +"个，具体如下：");
+//        for (Person p: people) {
+//            System.out.println("p = " + p.getName() + " \t" +p.getAge() );
+//        }
+        char[] word = new char[10]; //记录每个单词
+        int index = 0; //每个单词的下标
+        int num = 0; //单词数量
+        boolean flag = false;
+        String done = "done";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入单词 ");
+        while (scanner.hasNext()){
+            char[] line = scanner.nextLine().toCharArray();
+            for (char c : line) {
+                if (c == ' ' || c == '\n' || c == '\t') {
+                    String temp = String.valueOf(word).trim();
+                    if (temp.equals(done)) { //比较当前单词是否是done
+                        flag = true;
+                        break;
+                    } else {
+                        word = new char[10];
+                        index = 0;
+                        num++;
+                    }
+                } else {
+                    word[index] = c;
+                    index++;
+                }
+            }
+            if (flag){
+                break;
+            }
         }
+        System.out.println("输入的单词有 = " + num + "个");
     }
+
+
 }
 
-class AtomicDemo implements Runnable{
-    //	private volatile int serialNumber = 0;
-    private AtomicInteger serialNumber = new AtomicInteger(0);
+class Person {
 
-    @Override
-    public void run() {
-        try {
-            Thread.sleep(200);
-        } catch (InterruptedException e) {
-        }
-        System.out.print(getSerialNumber()+" ");
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
     }
-    public int getSerialNumber(){
-        return serialNumber.getAndIncrement();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
     }
 }
